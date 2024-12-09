91badc51bc4c9532d9abe1cd84354613f7ee3227
Criiss22g <criiss22g@gmail.com>
2024-12-09 13:20:27 +0100

    correccion de errores minimos

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 //el sprite es sacado de internet por eso los saltos del eje X son variados y no sigue un patrón de distancia
-const posiciones = ["-55px", "-110px", "-150px","-205px", "-255px", "-310px", "-360px"];
+const posiciones = ["0","-55px", "-110px", "-150px","-205px", "-255px", "-310px", "-360px"];
 
 const sprite = document.getElementById("sprite");
 
 let i = 0;
 function movimiento(){
-if (i == 5 || i == 1) sprite.style.width = "40px";
+if (i == 6 || i == 2) sprite.style.width = "40px";
 else sprite.style.width = "55px"
 sprite.style.backgroundPositionX = posiciones[i];
 i++;
 if (i == posiciones.length) i = 0; 
 }
 
-setInterval(movimiento, 200);
+setInterval(movimiento, 150);
